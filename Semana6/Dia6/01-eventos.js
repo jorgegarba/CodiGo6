8fc055e1b62156d91a80df2a1d82fa756db34876
Osmar Montesinos <osmarmontes50@gmail.com>
2019-07-06 09:48:10 -0500

    Update 2 Dia 6

--- Semana6/Dia6/01-eventos.js
+++ Semana6/Dia6/01-eventos.js
@@ -21,6 +21,52 @@ function Alerta(){
 //     alert("Otra alerta pero asignada a una variable");
 // }
 //2da manera es recibir el evento y como 2do parámetro es recibir el nombre de la función a ejecutar pero solo referenciando el nombre, sin () al final
-divRojo.addEventListener("click",Alerta);
+//Modelo de Eventos Standart
+divRojo.addEventListener("click",Alert;
+
+//------------------------------
 
 var divBlanco = document.getElementById("blanco");
+
+//Hacemos referencia directamente al evento del elemento
+//Modelo de eventos Básico
+//Hacemos regerencia y su evento directamente => elemento.evento = funcion anonima
+//a comparación de los eventos con addEventListener, se le agregará el prefijo on => ej.: onclick
+divBlanco.onclick = function(){
+    console.error("Hola soy un error de JS :)");
+}
+
+//--------------------------------
+
+var texto = function(){
+    console.log("Has hecho click al botón");
+}
+
+var texto2 = function(){
+    console.log("Has hecho click al botón y a ejecutado otra función");
+}
+
+var btnDocumento = document.getElementById("boton");
+
+btnDocumento.addEventListener("click",texto);
+btnDocumento.addEventListener("click",texto2);
+
+//removeEventListener removerá la ejecución de un evento, y su función => removeEventListener(evento,funcion);
+btnDocumento.removeEventListener("click",texto);
+
+var entrada = document.getElementById("input");
+
+//el evento focus hace referencia a un elemento de tipo input, cuando el cursor esta activo dentro.
+// entrada.addEventListener("focus",function(){
+//     console.log("Me has hecho focus");
+// });
+
+//el evento blur se activará cuando se quite el focus (marcador de escritura) de un elemento input
+// entrada.addEventListener("blur",function(){
+//     console.log("Me estoy ejecutando al salir");
+// });
+
+//el evento change se activará solamente cuando el contenido del input cambie, mientras no haya cambios no pasará nada!
+entrada.addEventListener("change",function(){
+    console.log("He cambiado!");
+});
