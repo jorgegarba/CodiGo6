d6acacbf8d1b7e04a535b6c2650ebf32169a97cc
Osmar Montesinos <osmarmontes50@gmail.com>
2019-07-26 20:47:35 -0500

    Tarea con Búsqueda

--- Semana9/Dia5/tareaTabla.js
+++ Semana9/Dia5/tareaTabla.js
@@ -1,6 +1,7 @@
 window.onload = function(){
 
     var tbody = document.getElementById("tbody");
+    var inputBuscar = document.getElementById("buscar");
 
     var personas = [
         {
@@ -34,4 +35,57 @@ window.onload = function(){
             edad:42
         }
     ];
+
+
+    function rellenarCeldas(arregloPersonas){
+        //Var temporal
+        var personasConsideradas = [];
+        //evaluamos si recibimos una arreglo de personas
+        if(arregloPersonas != null){
+            personasConsideradas = arregloPersonas;
+            //Y si no que ejecute todas las personas
+        }else{
+            personasConsideradas = personas;
+        }
+        // limpiamos la casa antes de poner cosas
+        var tr = "";
+        tbody.innerHTML = "";
+        //Renderiza las personasConsideradas
+        personasConsideradas.forEach(function(persona){
+            tr = tr + `<tr>
+                            <td>${persona.id}</td>
+                            <td>${persona.nombres}</td>
+                            <td>${persona.apellidos}</td>
+                            <td>${persona.edad}</td>
+                        </tr>`;
+        });
+        tbody.innerHTML = tr;
+    }
+    rellenarCeldas();
+
+    // var numeros = [6,5,6,7,4,3,5,4,5,3];
+
+    // var numfiltrados = numeros.filter(function(numero){
+    //    return numero == 6;
+    // })
+    // console.log("mis numeros filtrados son: " + numfiltrados);
+
+
+    inputBuscar.onkeyup = function(e){
+        // console.log("THIS",this.value);
+        console.log(e.target.value);
+
+        var textoFiltrado = this.value;
+        
+        var filtrado = personas.filter(function(persona){
+            console.log(toString(persona.edad));
+            // var texto = "Hola";
+            // texto.toLowerCase => hola
+            return persona.nombres.toLowerCase().includes(textoFiltrado.toLowerCase()) || persona.apellidos.toLowerCase().includes(textoFiltrado.toLowerCase()) ||  persona.edad == (parseInt(textoFiltrado)); //devuelve un true o false
+        }); //me va a devolver un arreglo con todo lo que haya filtrado
+
+        //filtrado = arreglo de objetos ya filtrado con el código de arriba
+        rellenarCeldas(filtrado);
+    }
+
 }
