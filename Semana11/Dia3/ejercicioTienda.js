b5d4e411fc5dddb97adb614c92cf21431667c406
Osmar Montesinos <osmarmontes50@gmail.com>
2019-08-07 21:34:25 -0500

    Update 2 Dia 3

--- Semana11/Dia3/ejercicioTienda.js
+++ Semana11/Dia3/ejercicioTienda.js
@@ -7,19 +7,45 @@ window.onload = function(){
     var precioProducto = document.getElementById("precioProducto");
     var stockProducto = document.getElementById("stockProducto");
     
+    function imprimirDetalle(id){
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function(){
+            switch (xhr.readyState){
+                case 4:
+                    console.log(xhr.responseText);
+                    let objProducto = JSON.parse(xhr.responseText);
+                    console.log(objProducto);
+
+                    nombreProducto.innerHTML = objProducto.prod_nom;
+
+                    descProducto.innerHTML = objProducto.prod_desc;
+
+                    precioProducto.innerHTML = objProducto.prod_prec;
+
+                    stockProducto.innerHTML = objProducto.prod_stock ? "Hay Stock" : "No hay Stock";
+                break;
+            }
+        }
+
+        xhr.open("GET","https://5d4b381a00dbb10014879959.mockapi.io/productos/"+id);
+        xhr.send(null);
+    }
+
     function imprimirProductos(arregloProductos){
         divProductos.innerHTML = "";
         divFila = document.createElement("div");
         divFila.setAttribute("class","row");
         divProductos.appendChild(divFila);
+
         for(let i=0;i<arregloProductos.length;i++){
             
             var divColumna = document.createElement("div");
-            divColumna.setAttribute("class","col-4");
-
+            divColumna.setAttribute("class","col-md-4");
+            
             divColumna.style.height = "300px";
             divColumna.style.backgroundImage = `url('${arregloProductos[i].prod_img}')`;
-            divColumna.style.margin = "2px";
+            
             // validamos si el producto esta en stock, en caso no este lo resaltamos con una borde
             if(arregloProductos[i].prod_stock == false){
                 divColumna.style.border = "2px solid red";
