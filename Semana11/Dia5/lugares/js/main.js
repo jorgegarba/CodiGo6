fd298220f82a47f103a1037be737d0e34e88a89f
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-08-12 21:41:03 -0500

    Subiendo API KEY

--- Semana11/Dia5/lugares/js/main.js
+++ Semana11/Dia5/lugares/js/main.js
@@ -4,11 +4,18 @@ window.onload = function () {
     var inputBuscar = document.getElementById("inputBuscar");
     var frmBusqueda = document.getElementById("frmBusqueda");
     var divVacio = document.getElementById("divVacio");
+    var cuerpo = document.getElementById("cuerpo");
+    var contenedorTabla = document.getElementById("contenedorTabla");
+    var map;
 
     frmBusqueda.onsubmit = function (evento) {
         evento.preventDefault();
         // ocultando el div de "sin resultados"
-        divVacio.setAttribute("hidden","true");
+        divVacio.setAttribute("hidden", "true");
+        // limpiar el cuerpo de la tabla
+        cuerpo.innerHTML = "";
+        // ocultar contenedor de la tabla
+        contenedorTabla.setAttribute("hidden", "true");
 
         var busqueda = inputBuscar.value;
         buscarLugar(busqueda);
@@ -18,7 +25,7 @@ window.onload = function () {
     function buscarLugar(busqueda) {
         // mostrando el div de carga
         divCarga.removeAttribute("hidden");
-        
+
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4) {
@@ -28,25 +35,80 @@ window.onload = function () {
                 dibujarTabla(gata);
             }
         }
-        
+
         xhr.open("GET", `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${busqueda}`);
         xhr.setRequestHeader("x-rapidapi-host", "devru-latitude-longitude-find-v1.p.rapidapi.com");
         xhr.setRequestHeader("x-rapidapi-key", "dc8d31b41amshedb873d01ae14adp10f91cjsnd24bc4353e07");
         xhr.send();
-        
+
     }
-    
-    function dibujarTabla(gata){
-        if(gata.length === 0 ){
+
+    function dibujarTabla(gata) {
+        if (gata.length === 0) {
             // no hay resultados
             divVacio.removeAttribute("hidden");
-        }else{
+        } else {
             // si hay resultados
-            var tabla = document.createElement("table");
-            var thead = document.createElement("thead");
-            tabla.appendChild(thead);
+
+            gata.forEach(function (elemento, i) {
+                var tr = document.createElement("tr");
+                var tdNro = document.createElement("td");
+                tdNro.innerHTML = (i + 1);
+                tr.appendChild(tdNro);
+
+                var tdNombre = document.createElement("td");
+                tdNombre.innerHTML = elemento.name;
+                tr.appendChild(tdNombre);
+
+                var tdTipo = document.createElement("td");
+                tdTipo.innerHTML = elemento.type;
+                tr.appendChild(tdTipo);
+
+                var tdPais = document.createElement("td");
+                tdPais.innerHTML = elemento.c;
+                tr.appendChild(tdPais);
+
+                var tdLongitud = document.createElement("td");
+                tdLongitud.innerHTML = elemento.lon;
+                tr.appendChild(tdLongitud);
+
+                var tdLatitud = document.createElement("td");
+                tdLatitud.innerHTML = elemento.lat;
+                tr.appendChild(tdLatitud);
+
+                var botonVer = document.createElement("button");
+                botonVer.innerHTML = "<i class='fas fa-eye'></i>";
+                botonVer.setAttribute("class", "btn btn-primary")
+                // botonVer.setAttribute("type","button");
+                // botonVer.setAttribute("data-toggle","modal");
+                // botonVer.setAttribute("data-target","#modalMapa");
+                botonVer.onclick = function () {
+                    $('#modalMapa').modal('show');
+                };
+
+                tr.appendChild(botonVer);
+
+
+                cuerpo.appendChild(tr);
+            });
+
+            contenedorTabla.removeAttribute("hidden");
+
         }
     }
 
+    function initMap() {
+        // inicializar el mapa
+        // la variable map es la referencia al mapa de google
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8
+        });
+    }
+
+    initMap();
+
 
 }
+
+
