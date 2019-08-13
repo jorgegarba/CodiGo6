8ade518eb602d5df77c45b21e4ccc6f45a78c0f9
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-08-12 19:27:03 -0500

    analisis de resultados

--- Semana11/Dia5/lugares/js/main.js
+++ Semana11/Dia5/lugares/js/main.js
@@ -3,9 +3,13 @@ window.onload = function () {
     var divCarga = document.getElementById("divCarga");
     var inputBuscar = document.getElementById("inputBuscar");
     var frmBusqueda = document.getElementById("frmBusqueda");
+    var divVacio = document.getElementById("divVacio");
 
     frmBusqueda.onsubmit = function (evento) {
         evento.preventDefault();
+        // ocultando el div de "sin resultados"
+        divVacio.setAttribute("hidden","true");
+
         var busqueda = inputBuscar.value;
         buscarLugar(busqueda);
     }
@@ -14,17 +18,35 @@ window.onload = function () {
     function buscarLugar(busqueda) {
         // mostrando el div de carga
         divCarga.removeAttribute("hidden");
-
+        
         var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState == 4){
-                divCarga.setAttribute("hidden","true");
-                console.log(JSON.parse(xhr.responseText));
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                divCarga.setAttribute("hidden", "true");
+                var gata = JSON.parse(xhr.responseText).Results;
+                console.log(gata);
+                dibujarTabla(gata);
             }
         }
-        xhr.open("GET",`https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${busqueda}`);
-        xhr.setRequestHeader("x-rapidapi-host","devru-latitude-longitude-find-v1.p.rapidapi.com");
-        xhr.setRequestHeader("x-rapidapi-key","dc8d31b41amshedb873d01ae14adp10f91cjsnd24bc4353e07");
+        
+        xhr.open("GET", `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${busqueda}`);
+        xhr.setRequestHeader("x-rapidapi-host", "devru-latitude-longitude-find-v1.p.rapidapi.com");
+        xhr.setRequestHeader("x-rapidapi-key", "dc8d31b41amshedb873d01ae14adp10f91cjsnd24bc4353e07");
         xhr.send();
+        
     }
+    
+    function dibujarTabla(gata){
+        if(gata.length === 0 ){
+            // no hay resultados
+            divVacio.removeAttribute("hidden");
+        }else{
+            // si hay resultados
+            var tabla = document.createElement("table");
+            var thead = document.createElement("thead");
+            tabla.appendChild(thead);
+        }
+    }
+
+
 }
