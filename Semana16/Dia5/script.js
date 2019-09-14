60af6a4d02d0fd64a7387db05bee9fa90a20c9e0
Osmar Montesinos <osmarmontes50@gmail.com>
2019-09-13 20:50:35 -0500

    Update3

--- Semana16/Dia5/script.js
+++ Semana16/Dia5/script.js
@@ -1,6 +1,9 @@
 window.onload = () => {
     var mapaGoogle;
+    var miPosicion;
     var divMapa = document.getElementById("mapa");
+    var btnGetPosition = document.getElementById("btnGetPosition");
+    
     let configurarMapa = () => {
         //google.maps.Map(elemento donde se colocara el mapa)
         mapaGoogle = new google.maps.Map(divMapa,{
@@ -8,6 +11,29 @@ window.onload = () => {
             zoom: 8
         });
     }
-    
+
+    btnGetPosition.onclick = () => {
+        //Esta habilitada la ubicación?
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition((posicion)=>{
+                console.log(posicion)
+                let ubicacion = {
+                    lat: posicion.coords.latitude,
+                    lng: posicion.coords.longitude
+                }
+                miPosicion = new google.maps.Marker({
+                    position: ubicacion,
+                    title:"Aqui estamos",
+                    map: mapaGoogle
+                });
+            },(error)=>{
+                console.log(error);
+            });
+        }else{
+            alert("No se permitio el acceso a la ubicación");
+        }
+    }
+
     configurarMapa();
+
 }
