59a9c554628940ab1838c46d4be82ca75afd0b1b
Osmar Montesinos <osmarmontes50@gmail.com>
2019-09-25 18:43:49 -0500

    Update 1

---
+++ Semana18/Dia1/angular-demo/src/app/classes/producto.ts
@@ -0,0 +1,5 @@
+export class Producto{
+    id:number;
+    nombre:string;
+    descripcion:string;
+}
--- Semana18/Dia1/angular-demo/src/app/components/formulario/formulario.component.ts
+++ Semana18/Dia1/angular-demo/src/app/components/formulario/formulario.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Producto } from '../../classes/producto';
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
-
   titulo: string = 'Agregar Productos';
-  
-  nombre: string = '';
+  producto: Producto = {
+    id: 1,
+    nombre: 'Pizza',
+    descripcion: 'Descripción de mi Pizza :)'
+  }
 
   arrayProductos: Array<any> = [];
 
   miFuncion(): void {
-    
-    this.arrayProductos.push(this.nombre);
+    this.arrayProductos.push(this.producto);
     console.log(this.arrayProductos);
-
   }
   constructor() { }
 
