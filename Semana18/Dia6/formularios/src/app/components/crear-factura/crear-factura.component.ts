17dfbaf07be0fae21173215db89611da51a32e87
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-10-11 19:53:17 -0500

    Sweet alert

--- Semana18/Dia6/formularios/src/app/components/crear-factura/crear-factura.component.ts
+++ Semana18/Dia6/formularios/src/app/components/crear-factura/crear-factura.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FacturasService } from './../../services/facturas.service';
 import { Subscription } from 'rxjs';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-crear-factura',
@@ -17,16 +20,40 @@ export class CrearFacturaComponent implements OnInit, OnDestroy {
     fact_ruc: ''
   }
 
-  constructor(private _sFactura: FacturasService) { }
+  constructor(private _sFactura: FacturasService,
+    private _sRouter: Router) { }
 
   ngOnInit() {
   }
 
   crearFactura() {
-    console.log(this.objFactura);
-    this.subscriptor = this._sFactura.postFactura(this.objFactura).subscribe((rpta) => {
-      console.log(rpta);
+    Swal.fire({
+      title: 'Espere un momento',
+      text: 'Estamos registrando la factura',
+      type: 'info',
+      allowOutsideClick: false,
+      showConfirmButton: false
     })
+
+    this.subscriptor = this._sFactura.postFactura(this.objFactura)
+      .subscribe((rpta) => {
+        if (rpta.id) {
+          // si tiene un campo id asignado, implica que el objeto fue creado
+          Swal.fire({
+            title: 'Éxito!',
+            type: 'success',
+            text: 'La factura ha sido creada con éxito mafren!',
+            confirmButtonText: 'Ir a Facturas',
+            allowOutsideClick: false
+          }).then((result) => {
+            if (result.value) {
+              this._sRouter.navigate(['facturas']);
+            }
+          })
+
+        }
+      })
+
   }
 
   ngOnDestroy() {
