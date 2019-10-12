53000a63fc0645cd59a8cd5b4b57fe64c9dda576
Osmar Montesinos <osmarmontes50@gmail.com>
2019-10-12 14:35:10 -0500

    Dia6 Delete con ForkJoin
    En factura.service.ts Para que que arregloRespuestas no se quede pegado con las peticiones anteriores, he creado un arregloTmp que contengas las peticiones y luego arregloRespuestas lo dejo Vacio asi solucionando el error de no poder eliminar varias veces en Lote

--- Semana18/Dia6/formularios/src/app/components/facturas/facturas.component.ts
+++ Semana18/Dia6/formularios/src/app/components/facturas/facturas.component.ts
@@ -23,6 +23,8 @@ export class FacturasComponent implements OnInit, OnDestroy {
     fact_ruc: ''
   }
 
+  facturasSeleccionadas: Array<any> = [];
+  
   constructor(private _sFacturas: FacturasService,
     private _sRouter: Router) { }
 
@@ -35,6 +37,9 @@ export class FacturasComponent implements OnInit, OnDestroy {
       .subscribe((resultado) => {
         this.facturas = resultado;
       });
+      console.log(this.facturasSeleccionadas)
+      this.facturasSeleccionadas = [];
+      console.log(this.facturasSeleccionadas)
   }
 
 
@@ -96,11 +101,7 @@ export class FacturasComponent implements OnInit, OnDestroy {
         $("#modalEditar").modal("show");
       }
     })
-
-
   }
-
-
   guardarCambios() {
     // consumir el servicio para editar la factura
     this._sFacturas.putFacturaById(this.objFactura).subscribe((rpta) => {
@@ -109,6 +110,52 @@ export class FacturasComponent implements OnInit, OnDestroy {
         this.traerFacturas();
         $("#modalEditar").modal("hide");
 
+      }
+    })
+  }
+
+  seleccionarFactura(evento, factura){
+    if(evento.target.checked){
+      this.facturasSeleccionadas.push(factura);
+      console.log(this.facturasSeleccionadas);
+    } else {
+      for (let i = 0; i < this.facturasSeleccionadas.length; i++){
+        if(factura.id === this.facturasSeleccionadas[i].id){
+          //array.splice(posicion,cant_elementos);
+      
+          this.facturasSeleccionadas.splice(i, 1);
+          console.log(this.facturasSeleccionadas);
+        }
+      }
+    }
+  }
+
+  eliminarFacturas(){
+    console.log("eliminarFactras",this.facturasSeleccionadas)
+    Swal.fire({
+      title: 'ar yu chur de cargarse estas facturas?',
+      text: 'El proceso no tiene vuelta atrás!',
+      type: 'warning',
+      confirmButtonText: 'Sí, borrar!',
+      showCancelButton: true,
+      cancelButtonText: 'No, cancelar!',
+    }).then((result) => {
+      if (result.value) {
+        this._sFacturas.deleteFacturas(this.facturasSeleccionadas).subscribe((rpta) => {
+          
+          if (rpta[0].id) {
+            Swal.fire({
+              position: 'top-end',
+              type: 'success',
+              title: 'Las facturas ha sido borrada con éxito',
+              showConfirmButton: false,
+              timer: 1500
+            });
+            
+            this.traerFacturas();
+          }
+          console.log(rpta);
+        })
       }
     })
   }
--- Semana18/Dia6/formularios/src/app/services/facturas.service.ts
+++ Semana18/Dia6/formularios/src/app/services/facturas.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { URL_BACKEND } from './../../environments/environment.prod';
+import { forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacturasService {
 
+  arregloRespuestas: Array<any> = [];
+  arregloTmp:Array<any> = [];
   constructor(private _sHttp: HttpClient) { }
 
   getFacturas(): Observable<any> {
@@ -37,6 +40,20 @@ export class FacturasService {
 
     return this._sHttp.put(`${URL_BACKEND}/facturas/${objFactura.id}`,
                             objFacturaString, { headers: misHeaders });
+  }
+
+  deleteFacturas(arregloFacturas): Observable<any>{
+    
+    arregloFacturas.forEach(factura => {
+      const respuesta = this._sHttp.delete(`${URL_BACKEND}/facturas/${factura.id}`);
+    
+      this.arregloRespuestas.push(respuesta);
+      this.arregloTmp = this.arregloRespuestas;
+      this.arregloRespuestas = [];
+    });
+    
+    return forkJoin(this.arregloTmp);
+
   }
 
 }
