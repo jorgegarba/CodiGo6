5d31502f22fb93ecdc8e30da0301d0971b5dd28d
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2019-11-08 19:12:13 -0500

    avance viernes

--- Semana22/01-basics/src/App.js
+++ Semana22/01-basics/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
-
+import Formulario from './components/Formulario';
 function App() {
+  let mascotas = [
+    {
+      nombre:'Scooby', 
+      tipo:'Perro'
+    },
+    {
+      nombre:'Garfield',
+      tipo:'Gato'
+    }
+  ]
   return (
     <div className="App">
+      <Formulario/>
     </div>
   );
 }
---
+++ Semana22/01-basics/src/components/Formulario.js
@@ -0,0 +1,20 @@
+// imrc => importa la clase React y Component de React
+import React, { Fragment } from 'react';
+
+// sfc => crea automaticamente un Stateless Functional Component
+const Formulario = (props) => {
+    // Todo componente react obligatoriamente tiene que retorna algo
+    let titulo = "Titulo del Formulario"
+    // En react solo se puede retornar un elemento, pero para que no afecte nuestros estilos utilizamos una etiqueta de REACT llamada Fragment
+    return (
+        <Fragment>
+            <h2>{titulo}</h2>
+            <h4>Subtitulo</h4>
+            <form>
+                <input type="text"/>
+            </form>
+        </Fragment>
+    );
+}
+
+export default Formulario;
