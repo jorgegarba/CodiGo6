c2365cbc339e87882b1fb94eeba42bc34edded96
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2019-11-11 19:03:12 -0500

    avance lunes

--- Semana22/02-statecomponents/src/App.js
+++ Semana22/02-statecomponents/src/App.js
@@ -1,35 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-
-
-const Componente = (props) => {
-
-  return (
-    <div>
-      <button onClick={() => {
-        console.log("cambiar");
-        props.cambiar90(800);
-      }}>
-        click
-      </button>
-      <p>{props.t}</p>
-    </div>
-  );
-}
-
-function App() {
-  let x = 90;
-  let cambiar90 = (valor) => {
-    console.log("cambiando valor");
-    x = valor;
+class App extends Component {
+  // como toda clase en JS, podemos tener un constructor para inicializar las variables
+  // En una clase las props(propiedades) se reciben por medio del constructor
+  constructor(props){
+    // Se usa el super para hacer referencia a nuestra clase padre (Component) para pasarle nuestras propiedas, esto se usa mas que todo para usar Hooks, no podemos usar "this" hasta pasar la referencia a nuestro padre
+    // ACA NO SE PUEDE USAR THIS
+    super(props);
+    // ACA SI SE PUEDE USAR THIS
+    // Para modificar nuestro state y como estamos en una clase usamos el this
+    this.state={
+      productos:[
+        {id:1,nombre:'Leche',precio:4.20},
+        {id:2,nombre:'Arroz',precio:2.10},
+        {id:3,nombre:'Manjar',precio:5.30}
+      ],
+      producto:{}
+    }
   }
-  return (
-    <div >
-      <Navbar marca={"CodiGo"} />
-      <p>{x}</p>
-      <Componente t={1} cambiar90={cambiar90} />
-    </div>
-  );
+  // A diferencia de un functional component, una class component tiene que tener el metodo render () y dentro de el, el retorno de lo que se va a mostrar (codigo html)
+  render() {
+    return (
+      <div >
+        <Navbar marca={"CodiGo"} />
+      </div>
+
+    );
+  }
 }
 
 export default App;
